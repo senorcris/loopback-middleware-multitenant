a6c8841cc352bbbd55854be2af2ffa1f5ad40d31
senorcris <cristiantav@gmail.com>
2016-02-06 11:54:01 -0500

    removing console log; test change

--- test/scope.test.js
+++ test/scope.test.js
@@ -23,90 +23,94 @@ describe('Scope', function() {
     sandbox.restore();
   });
 
-  it('should create the property `tenantId` for a model -- hiddenProperties', function() {
-    var ModelStub = sandbox.stub({
-      definition: {
-        name: 'ModelStub',
-        settings: {
-          hiddenProperties: {}
-        }
-      },
-      defineProperty: function(){}
+  describe('extendWithTenantId', function() {
+    it('should create the property `tenantId` for a model -- hiddenProperties', function() {
+      var ModelStub = sandbox.stub({
+        definition: {
+          name: 'ModelStub',
+          settings: {
+            hiddenProperties: {}
+          }
+        },
+        defineProperty: function(){}
+      });
+      multitenantScope.extendWithTenantId(ModelStub);
+      expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
+        'type': String,
+        'required': true
+      });
+      expect(ModelStub.definition.settings.hiddenProperties).to.contain.key('tenantId');
+      expect(ModelStub.definition.settings.hiddenProperties.tenantId).to.be.true;
     });
-    multitenantScope.extendWithTenantId(ModelStub);
-    expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
-      'type': String,
-      'required': true
-    });
-    expect(ModelStub.definition.settings.hiddenProperties).to.contain.key('tenantId');
-    expect(ModelStub.definition.settings.hiddenProperties.tenantId).to.be.true;
-  });
 
-  it('should create the property `tenantId` for a model -- hidden', function() {
-    var ModelStub = sandbox.stub({
-      definition: {
-        name: 'ModelStub',
-        settings: {
-          hidden: []
-        }
-      },
-      defineProperty: function(){}
+    it('should create the property `tenantId` for a model -- hidden', function() {
+      var ModelStub = sandbox.stub({
+        definition: {
+          name: 'ModelStub',
+          settings: {
+            hidden: []
+          }
+        },
+        defineProperty: function(){}
+      });
+      multitenantScope.extendWithTenantId(ModelStub);
+      expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
+        'type': String,
+        'required': true
+      });
+      expect(ModelStub.definition.settings.hidden).to.contain('tenantId');
     });
-    multitenantScope.extendWithTenantId(ModelStub);
-    expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
-      'type': String,
-      'required': true
-    });
-    expect(ModelStub.definition.settings.hidden).to.contain('tenantId');
-  });
 
-  it('should create the property `tenantId` for a model', function() {
-    var ModelStub = sandbox.stub({
-      definition: {
-        name: 'ModelStub',
-        settings: {}
-      },
-      defineProperty: function(){}
+    it('should create the property `tenantId` for a model', function() {
+      var ModelStub = sandbox.stub({
+        definition: {
+          name: 'ModelStub',
+          settings: {}
+        },
+        defineProperty: function(){}
+      });
+      multitenantScope.extendWithTenantId(ModelStub);
+      expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
+        'type': String,
+        'required': true
+      });
+      expect(ModelStub.definition.settings.hidden).to.contain('tenantId');
     });
-    multitenantScope.extendWithTenantId(ModelStub);
-    expect(ModelStub.defineProperty).to.have.been.calledWith('tenantId', {
-      'type': String,
-      'required': true
+  })
+
+  describe('limitReadToTenant', function() {
+    it('should call next with an error when tenant is not defined', function() {
+      var ctx = sandbox.stub();
+      var next = sandbox.stub();
+      multitenantScope.limitReadToTenant(ctx, next);
+      expect(next).to.have.been.called;
     });
-    expect(ModelStub.definition.settings.hidden).to.contain('tenantId');
-  });
 
-  it('should call next with an error when tenant is not defined', function() {
-    var ctx = sandbox.stub();
-    var next = sandbox.stub();
-    multitenantScope.limitReadToTenant(ctx, next);
-    expect(next).to.have.been.called;
-  });
-
-  it('should call next when the tenant mixin has been extended', function() {
-    var ctx = sandbox.stub({
-      hookState: {},
-      query: {}
+    it('should call next when the tenant mixin has been extended', function() {
+      var ctx = sandbox.stub({
+        hookState: {},
+        query: {}
+      });
+      var next = sandbox.stub();
+      getStub.onCall(0).returns('tenantOne');
+      getStub.onCall(1).returns({ sharedDataSource: true });
+      getStub.onCall(2).returns('tenantOne');
+      getStub.onCall(3).returns({ sharedDataSource: true });
+      multitenantScope.limitReadToTenant(ctx, next);
+      multitenantScope.limitReadToTenant(ctx, next);
+      expect(ctx.hookState.tenantMixinWhereExtended).to.be.true;
+      expect(next).to.have.been.calledTwice;
     });
-    var next = sandbox.stub();
-    getStub.onCall(0).returns('tenantOne');
-    getStub.onCall(1).returns({ sharedDataSource: true });
-    getStub.onCall(2).returns('tenantOne');
-    getStub.onCall(3).returns({ sharedDataSource: true });
-    multitenantScope.limitReadToTenant(ctx, next);
-    multitenantScope.limitReadToTenant(ctx, next);
-    expect(ctx.hookState.tenantMixinWhereExtended).to.be.true;
-    expect(next).to.have.been.calledTwice;
-  });
 
-  it('should limit all queries to a specific tenantId', function() {
-    var ctx = sandbox.stub({ hookState: {}, query: {} });
-    var next = sandbox.stub();
-    getStub.onCall(0).returns('tenantOne');
-    getStub.onCall(1).returns({ sharedDataSource: true });
-    multitenantScope.limitReadToTenant(ctx, next);
-    expect(next).to.have.been.called;
-    expect(ctx.hookState.tenantMixinWhereExtended).to.be.true;
-    expect(ctx.query.where.tenantId).to.equal('tenantOne');
+    it('should limit all queries to a specific tenantId', function() {
+      var ctx = sandbox.stub({ hookState: {}, query: {} });
+      var next = sandbox.stub();
+      getStub.onCall(0).returns('tenantOne');
+      getStub.onCall(1).returns({ sharedDataSource: true });
+      multitenantScope.limitReadToTenant(ctx, next);
+      expect(next).to.have.been.called;
+      expect(ctx.hookState.tenantMixinWhereExtended).to.be.true;
+      expect(ctx.query.where.tenantId).to.equal('tenantOne');
+    });
   });
 });
